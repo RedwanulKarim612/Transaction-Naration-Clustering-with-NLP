409c9957b131acdf7391f5da2886cb3379a2c532
Md.Redwanul Karim <58245255+RedwanulKarim612@users.noreply.github.com>
2023-06-01 04:16:21 +0600

    Removing Names and Locations

--- PreprocessingUtil.py
+++ PreprocessingUtil.py
@@ -1,11 +1,25 @@
+import numpy as np
 import nltk
 from nltk.corpus import stopwords
+import spacy
 
 tokenizer = nltk.tokenize.WhitespaceTokenizer()
 lemmatizer = nltk.stem.WordNetLemmatizer()
 stopwords = stopwords.words('english')
 stopwords.append('mm')
 stopwords.append('c')
+stopwords.append('md')
+nlp = spacy.load('en_core_web_sm')
+def clean_naration(text):
+    parser = nlp(text)
+    cleaned = []
+    remove_types = ['PERSON', 'GPE', 'LOC']
+    for item in parser:
+        if item.ent_type in remove_types:
+            pass
+        else:
+            cleaned.append(item.text)
+    return (" ".join(cleaned))
 
 def remove_special_chars(sentence):
     result = sentence.replace('[^A-Za-z]', ' ', regex = True)
@@ -16,7 +30,7 @@ def remove_stop_words(sentence):
     return ' '.join(word.lower() for word in str(sentence).split() if word not in stopwords)
 
 def lemmatize(text):
-  return [lemmatizer.lemmatize(w) for w in tokenizer.tokenize(text)]
+    return [lemmatizer.lemmatize(w) for w in tokenizer.tokenize(text)]
 
 def sort_coo(coo_matrix):
     """Sort a dict with highest score"""
@@ -46,7 +60,7 @@ def extract_topn_from_vector(feature_names, sorted_items, topn=10):
     
     return results
 
-def get_keywords(vectorizer, feature_names, doc):
+def get_keywords(vectorizer, feature_names, doc, k=10):
     """Return top k keywords from a doc using TF-IDF method"""
 
     #generate tf-idf for the given document
@@ -56,7 +70,7 @@ def get_keywords(vectorizer, feature_names, doc):
     sorted_items=sort_coo(tf_idf_vector.tocoo())
 
     #extract only TOP_K_KEYWORDS
-    keywords=extract_topn_from_vector(feature_names,sorted_items,TOP_K_KEYWORDS)
+    keywords=extract_topn_from_vector(feature_names,sorted_items,k)
     
     return list(keywords.keys())
 
